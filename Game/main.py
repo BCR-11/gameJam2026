8feac1b3f3d73b1b259a050c9daa857871f8397f
Brian CR <brianjcl@uio.no>
2026-02-07 22:32:07 +0100

    Polished and added randomness to Fruit class

--- Game/main.py
+++ Game/main.py
@@ -1,23 +1,22 @@
-import pygame
-import sys
+import pygame, sys, random
 from pygame.math import Vector2
 
 pygame.init()
 
 #FRUITS
 class FRUIT:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.pos = Vector3(x, y)
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
 
     def draw_fruit (self):
-        fruit_rect = pygame.Rect(self.pos.x, self.pos.y, cell_size, cell_size)
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
         pygame.draw.rect(screen, ('#ff0048') , fruit_rect)
 
 #SCREEN 
 cell_size = 40
-cell_number = 20
+cell_number = 20 # A 20 by 20 grid, 40 pixels each square.
 screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 pygame.display.set_caption ("Snake")
 #FPS
@@ -33,6 +32,7 @@ speed = 8
 snake_surf = pygame.transform.rotozoom(pygame.image.load('sprites/snake_headUP.png'), 0, 2)
 snake_rect = snake_surf.get_rect(center = (x_pos, 360))
 
+fruit = FRUIT()
 
 #FONT FOR WRITING (Pixelated)
 pixel_font = pygame.font.Font('font/Minecraft.ttf', 80) # Directory + size
@@ -49,6 +49,7 @@ while running:
             running = False
     screen.fill((0, 0, 0)) #fill screen in black
     screen.blit(text, text_rect)
+    fruit.draw_fruit()
 
     keys = pygame.key.get_pressed()
     
